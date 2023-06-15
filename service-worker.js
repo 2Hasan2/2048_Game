946fa9aa5c05b00307f5c4c83bd9fd5ab746f48d
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-06-15 22:00:35 +0300

    CONFIG: ServiceWorker

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+ const CACHE_NAME = '2048-game-cache-v1';
+const urlsToCache = [
+  '/',
+  '/css.css',
+  '/script.js',
+  '/favicon.ico',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
